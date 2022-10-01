cedc26605f838701c6ebf9ebf6a41e955ab7f317
Nikita Bugrov <dustinman@mail.ru>
2022-10-01 23:27:02 +0300

    lite-server to esbuild server

--- build.js
+++
@@ -1,8 +0,0 @@
-require('esbuild').buildSync({
-    entryPoints: ['src/index.tsx'],
-    bundle: true,
-    minify: true,
-    sourcemap: true,
-    target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
-    outfile: 'build/build.js',
-  })
--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,7 @@ const App = () => (<div>
   <h1>Hello, world!</h1>
 </div>)
 
-let root: HTMLElement | null = document.getElementById('root');
-
-if (root === null) {
-    root = document.createElement('div');
-    root.id = 'root';
-}
+let root: HTMLElement = document.getElementById('root') as HTMLElement;
 
 const container = ReactDOM.createRoot(root);
 container.render(<App />);
